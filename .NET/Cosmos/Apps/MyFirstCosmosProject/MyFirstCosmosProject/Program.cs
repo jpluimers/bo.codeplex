053f38fd4058afc760ffdf98afadbf235837df80
Jeroen Wiert Pluimers <jpluimers@users.noreply.github.com>
2010-08-04 09:39:56 +0000

    Test if MS5 of Cosmos installs and builds the basic template correctly.

---
+++ .NET/Cosmos/Apps/MyFirstCosmosProject/MyFirstCosmosProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.User.Kernel
+{
+    public class Program
+    {
+	public static void Init()
+	{
+		Main();
+	}
+        static void Main()
+        {
+            // Boot the Cosmos kernel:
+            Cosmos.Sys.Boot xBoot = new Cosmos.Sys.Boot();
+            xBoot.Execute();
+            
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back:");
+            string xResult = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(xResult);
+        }
+    }
+}
