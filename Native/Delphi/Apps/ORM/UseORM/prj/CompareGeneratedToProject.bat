2df9f817d8485c1a34e928c59a84902552344df7
jeroenp_cp <SND\jeroenp_cp>
2010-09-23 17:53:35 +0000

    Don't keep CMD prompt open when comparing

--- Native/Delphi/Apps/ORM/UseORM/prj/CompareGeneratedToProject.bat
+++ Native/Delphi/Apps/ORM/UseORM/prj/CompareGeneratedToProject.bat
@@ -3,5 +3,5 @@ echo C:\temp\ORM\ORM
 echo C:\develop\codeplex\TFS05\bo\Native\Delphi\Apps\ORM\UseORM\src\ORM
 rem "C:\Program Files\Embarcadero\RAD Studio\8.0\bin\BCompareLite.exe" C:\temp\ORM\ORM C:\develop\codeplex\TFS05\bo\Native\Delphi\Apps\ORM\UseORM\src\ORM
 rem "C:\Program Files\Embarcadero\RAD Studio\8.0\bin\BCompareLite.exe"
-"C:\Program Files\Beyond Compare 3\BCompare.exe" C:\temp\ORM\ORM C:\develop\codeplex\TFS05\bo\Native\Delphi\Apps\ORM\UseORM\src\ORM
+start "compare" "C:\Program Files\Beyond Compare 3\BCompare.exe" C:\temp\ORM\ORM C:\develop\codeplex\TFS05\bo\Native\Delphi\Apps\ORM\UseORM\src\ORM
 
