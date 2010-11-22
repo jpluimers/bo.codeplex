144c002c9e4f2842f633a1308a409dd3ab86c2e4
jeroenp_cp <SND\jeroenp_cp>
2010-11-22 07:47:15 +0000

    LoopWithAnoymousLambda - https://gist.github.com/709647

---
+++ .NET/C#/Apps/AnonymousMethods/AnonymousMethodInLoopConsoleApplication/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnonymousMethodInLoopConsoleApplication
+{
+    class Program
+    {
+
+        static void LoopWithAnoymousMethod()
+        {
+            List<Action> methodList = new List<Action>();
+            for (int i = 0; i < 5; i++)
+            {
+                methodList.Add(
+                    delegate()
+                    {
+                        Console.WriteLine(i);
+                    }
+                    );
+            }
+            foreach (Action method in methodList)
+            {
+                method();
+            }
+        }
+
+        static void LoopWithAnoymousLambda()
+        {
+            List<Action> methodList = new List<Action>();
+            for (int i = 0; i < 5; i++)
+            {
+                methodList.Add(
+                    () =>
+                        Console.WriteLine(i)
+                );
+            }
+            foreach (Action method in methodList)
+            {
+                method();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("LoopWithAnoymousMethod:");
+                LoopWithAnoymousMethod();
+                Console.WriteLine("LoopWithAnoymousLambda:");
+                LoopWithAnoymousLambda();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.Write("Press <Enter>");
+            Console.ReadLine();
+        }
+    }
+}
