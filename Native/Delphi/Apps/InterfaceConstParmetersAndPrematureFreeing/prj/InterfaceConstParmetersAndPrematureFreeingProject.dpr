9fb40ec2f99ccc0e79296e4be701d8fa4e1eb679
jeroenp_cp <SND\jeroenp_cp>
2010-12-22 12:14:44 +0000

    improved InterfaceConstParmetersAndPrematureFreeingProject

--- Native/Delphi/Apps/InterfaceConstParmetersAndPrematureFreeing/prj/InterfaceConstParmetersAndPrematureFreeingProject.dpr
+++ Native/Delphi/Apps/InterfaceConstParmetersAndPrematureFreeing/prj/InterfaceConstParmetersAndPrematureFreeingProject.dpr
@@ -1,90 +1,150 @@
+{$define all}
+
 program InterfaceConstParmetersAndPrematureFreeingProject;
 
 {$APPTYPE CONSOLE}
 
 uses
   SysUtils,
-  Windows;
+  Windows,
+  MyInterfacedObjectUnit in '..\src\MyInterfacedObjectUnit.pas';
 
-type
-  // Adpoted copy of TInterfacedObject for debugging
-  TMyInterfacedObject = class(TObject, IInterface)
-  protected
-    FRefCount: Integer;
-    function QueryInterface(const IID: TGUID; out Obj): HResult; stdcall;
-    function _AddRef: Integer; stdcall;
-    function _Release: Integer; stdcall;
-  public
-    procedure AfterConstruction; override;
-    procedure BeforeDestruction; override;
-    class function NewInstance: TObject; override;
-    property RefCount: Integer read FRefCount;
-  end;
-
-procedure TMyInterfacedObject.AfterConstruction;
+procedure Dump(Reference: IInterface);
 begin
-  InterlockedDecrement(FRefCount);
+  Writeln('    4.Dump begin');
+  Writeln('    4.Dump Reference=', Integer(PChar(Reference)));
+  Writeln('    4.Dump end');
 end;
 
-procedure TMyInterfacedObject.BeforeDestruction;
-begin
-  if RefCount <> 0 then
-    System.Error(reInvalidPtr);
-end;
-
-class function TMyInterfacedObject.NewInstance: TObject;
-begin
-  Result := inherited NewInstance;
-  TMyInterfacedObject(Result).FRefCount := 1;
-end;
-
-function TMyInterfacedObject.QueryInterface(const IID: TGUID; out Obj): HResult;
-begin
-  if GetInterface(IID, Obj) then
-    Result := 0
-  else
-    Result := E_NOINTERFACE;
-end;
-
-function TMyInterfacedObject._AddRef: Integer;
-begin
-  Result := InterlockedIncrement(FRefCount);
-  Writeln('_AddRef ', FRefCount);
-end;
-
-function TMyInterfacedObject._Release: Integer;
-begin
-  Result := InterlockedDecrement(FRefCount);
-  Writeln('_Release ', FRefCount);
-  if Result = 0 then
-  begin
-    Writeln('_Release Destroy');
-    Destroy;
-  end;
-end;
+{$ifdef all}
 
 procedure NoLeak(Reference: IInterface);
 begin
-  Writeln('2.NoLeak begin');
-  Supports(Reference, IInterface); // now we do not leak because the non-const parameter has an implicit reference count
-  Writeln('2.NoLeak end');
+  Writeln('   3.NoLeak begin');
+  Dump(Reference); // now we do not leak because the non-const parameter has an implicit reference count
+  Writeln('   3.NoLeak Reference is still alive, so it is safe to access it');
+  Writeln('   3.NoLeak end');
 end;
 
 procedure Leak(const Reference: IInterface);
 begin
-  Writeln('2.Leak begin');
-  Supports(Reference, IInterface); // now we leak because the caller does not keep a reference to us
-  Writeln('2.Leak end');
+  Writeln('   3.Leak begin');
+  Dump(Reference); // now we leak because the caller does not keep a reference to us
+  Writeln('   3.Leak Reference got destroyed if it had a RefCount of 1 upon entry, so now it can be unsafe to access it');
+  Writeln('   3.Leak end');
+end;
+
+{$endif all}
+
+procedure LeakCrash(const Reference: IInterface);
+begin
+  Writeln('   3.LeakCrash begin');
+  try
+    Dump(Reference); // now we leak because the caller does not keep a reference to us
+    Writeln('   3.LeakCrash Reference got destroyed if it had a RefCount of 1 upon entry, so now it can be unsafe to access it');
+    Dump(Reference); // we might crash here
+  except
+    begin
+      Writeln('   3.LeakCrash end with exception');
+      raise;
+    end;
+  end;
+  Writeln('   3.LeakCrash end');
+end;
+
+{$ifdef all}
+
+function CreateIInterfaceInstance: IInterface;
+begin
+  Writeln('   3.CreateIInterfaceInstance begin');
+  Result := TMyInterfacedObject.Create;
+  Writeln('   3.CreateIInterfaceInstance begin');
+end;
+
+procedure RunNoLeak;
+begin
+  Writeln(' 2.RunNoLeak begin');
+  NoLeak(TMyInterfacedObject.Create);
+  Writeln(' 2.RunNoLeak end');
+end;
+
+procedure RunNoLeak_CreateIInterfaceInstance;
+begin
+  Writeln(' 2.RunNoLeak_CreateIInterfaceInstance begin');
+  NoLeak(CreateIInterfaceInstance);
+  Writeln(' 2.RunNoLeak_CreateIInterfaceInstance end');
+end;
+
+procedure RunLeak;
+begin
+  Writeln(' 2.RunLeak begin');
+  Leak(TMyInterfacedObject.Create);
+  Writeln(' 2.RunLeak end');
+end;
+
+procedure RunLeak_CreateIInterfaceInstance;
+begin
+  Writeln(' 2.RunLeak_CreateIInterfaceInstance begin');
+  Leak(CreateIInterfaceInstance);
+  Writeln(' 2.RunLeak_CreateIInterfaceInstance end');
+end;
+
+procedure RunLeakWithLocalVar();
+var
+  Reference: IInterface;
+begin
+  Writeln(' 2.RunLeakWithLocalVar begin');
+  Reference := TMyInterfacedObject.Create;
+  Leak(Reference);
+  Writeln(' 2.RunLeakWithLocalVar end');
+end;
+
+{$endif all}
+
+procedure RunLeakCrash;
+begin
+  Writeln(' 2.RunLeakCrash begin');
+  LeakCrash(TMyInterfacedObject.Create);
+  Writeln(' 2.RunLeakCrash end');
 end;
 
 procedure Run();
 begin
-  Writeln('1.Run begin');
-  Writeln('1.Run NoLeak');
-  NoLeak(TMyInterfacedObject.Create());
-  Writeln('1.Run Leak');
-  Leak(TMyInterfacedObject.Create());
-  Writeln('1.Run end');
+  try
+    Writeln('1.Run begin');
+
+{$ifdef all}
+
+    Writeln('');
+    Writeln('1.RunNoLeak');
+    RunNoLeak();
+
+    Writeln('');
+    Writeln('1.RunNoLeak_CreateIInterfaceInstance');
+    RunNoLeak_CreateIInterfaceInstance();
+
+    Writeln('');
+    Writeln('1.RunLeak');
+    RunLeak();
+
+    Writeln('');
+    Writeln('1.RunLeak_CreateIInterfaceInstance');
+    RunLeak_CreateIInterfaceInstance();
+
+    Writeln('');
+    Writeln('1.RunLeakWithLocalVar');
+    RunLeakWithLocalVar();
+
+{$endif all}
+
+    Writeln('');
+    Writeln('1.RunLeakCrash');
+    RunLeakCrash();
+
+  finally
+    Writeln('');
+    Writeln('1.Run end');
+  end;
 end;
 
 begin
