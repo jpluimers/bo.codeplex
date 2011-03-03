835da1bebb5a15137262e05a93339f1cff5db165
Jeroen Wiert Pluimers <jpluimers@users.noreply.github.com>
2011-03-03 17:08:20 +0000

    bo.Configuration

---
+++ .NET/C#/bo.Configuration/bo.Configuration.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Configuration;
+
+namespace bo.Configuration
+{
+    public class AppConfig
+    {
+        public static string Entry(string key)
+        {
+            string result = ConfigurationManager.AppSettings[key];
+            return result;
+        }
+
+        public static bool Entry(string key, bool defaultValue)
+        {
+            bool result = defaultValue;
+            Entry(key, ref result);
+            return result;
+        }
+
+        public static int Entry(string key, int defaultValue)
+        {
+            int result = defaultValue;
+            Entry(key, ref result);
+            return result;
+        }
+
+        public static bool Entry(string key, ref bool value)
+        {
+            bool? result;
+            Entry(key, out result);
+            bool haveResult = result.HasValue;
+            if (haveResult)
+                value = result.Value;
+            return haveResult;
+        }
+
+        public static bool Entry(string key, ref int value)
+        {
+            int? result;
+            Entry(key, out result);
+            bool haveResult = result.HasValue;
+            if (haveResult)
+                value = result.Value;
+            return haveResult;
+        }
+
+        public static void Entry(string key, out bool? value)
+        {
+            string entry = Entry(key);
+
+            if (string.IsNullOrEmpty(entry))
+                value = null;
+            else
+            {
+                bool actualValue;
+                if (bool.TryParse(entry, out actualValue))
+                    value = actualValue;
+                else
+                    value = null;
+            }
+        }
+
+        public static void Entry(string key, out int? value)
+        {
+            string entry = Entry(key);
+
+            if (string.IsNullOrEmpty(entry))
+                value = null;
+            else
+            {
+                int actualValue;
+                if (int.TryParse(entry, out actualValue))
+                    value = actualValue;
+                else
+                    value = null;
+            }
+        }
+
+    }
+}
