1d69744308c29cc1dbae833e0254c0fbf5e2c399
jeroenp_cp <SND\jeroenp_cp>
2011-04-30 21:19:45 +0000

    MijnAlbumNl

---
+++ .NET/C#/Apps/Internet/Web/MijnAlbum.NL.Download.ConsoleApplication/MijnAlbumNl.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MijnAlbum.NL.Download.ConsoleApplication
+{
+
+    // http://keeshink.blogspot.com/2008/09/fotos-downloaden-van-mijnalbumnl.html
+    public class MijnAlbumNl
+    {
+        protected const string ThumbnailPrefix = "http://www.mijnalbum.nl/index.php?m=albumview&a=2&key=";
+        protected const string BigJpegUrlMask = "http://www.mijnalbum.nl/GroteFoto-{0}.jpg";
+
+        private static string DownloadString(string url)
+        {
+            var html = string.Empty;
+            using (var webClient = new System.Net.WebClient())
+            {
+                html = webClient.DownloadString(url);
+            }
+            return html;
+        }
+
+        protected static string DownloadThumbnailsHtml(string AlbumID)
+        {
+            string url = GetThumbnailsUrl(AlbumID);
+            string html = DownloadString(url);
+            return html;
+        }
+
+        protected static string GetThumbnailsUrl(string AlbumID)
+        {
+            string url = ThumbnailPrefix + AlbumID;
+            return url;
+        }
+
+        protected static List<string> getPhotoIds(bool dumpGroups, string html)
+        {
+            List<string> photoIds = new List<string>();
+            /* find strings like these:
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-3ANGJPWW.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-6OKRE67M.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-4OSAXTJM.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-7MFJVO4F.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-YQ4BZ4AD.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-3LKQD6AG.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-O8RUL378.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-7TTFUUCG.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-L6SLLZHO.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-YLSWWFJI.jpg"));
+            thumbs.push(new Array("http://www.mijnalbum.nl/MiniFoto-CFWOY8KK.jpg"));
+                         * RegEx string:
+            thumbs\.push\(new\ Array\("http://www.mijnalbum.nl/MiniFoto-(?<PhotoId>.*?)\.jpg"\)\);
+                         */
+            // With @, you only have to escape the double quotes with another double quote: http://weblogs.asp.net/lorenh/archive/2004/09/30/236134.aspx
+            const string RegExPattern = @"thumbs\.push\(new\ Array\(""http://www.mijnalbum.nl/MiniFoto-(?<PhotoId>.*?)\.jpg\""\)\);";
+            const string PhotoIdRegExCaptureGroupName = "PhotoId";
+            //const string RegExReplacement = @"${PhotoId}";
+            MatchCollection matchCollection = Regex.Matches(html, RegExPattern);
+            // Matches uses lazy evaluation, so each match will be evaluated when it is accessed in the loop
+            foreach (Match match in matchCollection)
+            {
+                Group positionIdGroup = match.Groups[PhotoIdRegExCaptureGroupName];
+                if (dumpGroups)
+                    Console.WriteLine(positionIdGroup.Value);
+                photoIds.Add(positionIdGroup.Value);
+            }
+            return photoIds;
+        }
+
+        protected static string GetBigJpegUrl(string photoId)
+        {
+            string bigJpegUrl = string.Format(BigJpegUrlMask, photoId);
+            return bigJpegUrl;
+        }
+
+    }
+
+}
---
+++ .NET/C#/Apps/Internet/Web/MijnAlbum.NL.Download.ConsoleApplication/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MijnAlbum.NL.Download.ConsoleApplication
+{
+    class Program : MijnAlbumNl
+    {
+
+        static void Main(string[] args)
+        {
+            bool dumpHtml = false;
+            bool dumpGroups = false;
+
+            // http://www.mijnalbum.nl/Album=TYEGMIMD
+            const string AlbumID = "TYEGMIMD";
+
+            if (0 == args.Length)
+                args = new string[] { AlbumID };
+
+            foreach (string arg in args)
+            {
+                Console.WriteLine("Thumbnail URL for Album {0}", arg);
+                string url = GetThumbnailsUrl(arg);
+                Console.WriteLine(url);
+
+                List<string> photoIds = GetPhotoIds(dumpHtml, dumpGroups, arg);
+                Console.WriteLine("Photo URLs for Album {0}", arg);
+                foreach (string photoId in photoIds)
+                {
+                    string bigJpegUrl = GetBigJpegUrl(photoId);
+                    Console.WriteLine(bigJpegUrl);
+                }
+            }
+
+            Console.Write("Press <Enter>");
+            Console.ReadLine();
+        }
+
+        private static List<string> GetPhotoIds(bool dumpHtml, bool dumpGroups, string arg)
+        {
+            string html = DownloadThumbnailsHtml(arg);
+
+            if (dumpHtml)
+                Console.WriteLine(html);
+
+            // when it is not proper XHtml:
+            // http://stackoverflow.com/questions/100358/looking-for-c-html-parser
+            // http://stackoverflow.com/questions/5359805/problem-while-parsing-html-xml-and-quering-with-xpath
+            //for element in htmltree.xpath("body/table/tr/td[@class='thumbfoto']"):
+            //System.Xml.XmlDocument xmlDocument = new System.Xml.XmlDocument();
+            // alas: the html is not valid XML, so work around it with plain searches
+            //xmlDocument.LoadXml(html);
+            //xmlDocument.SelectNodes("");
+            //          string total = xmlDocument.SelectSingleNode("Surveys/Survey[@name='Beer']/cellName[@name='MatureBlueCollarMen']").Attributes.GetNamedItem("total").Value;
+
+            List<string> photoIds = getPhotoIds(dumpGroups, html);
+            return photoIds;
+        }
+
+    }
+}
