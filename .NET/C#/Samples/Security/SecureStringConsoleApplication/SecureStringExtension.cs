a2e4f850f8812722277bee801966c9e641dd6982
jeroenp_cp <SND\jeroenp_cp>
2011-07-12 18:28:11 +0000

    SecureString demo

---
+++ .NET/C#/Samples/Security/SecureStringConsoleApplication/SecureStringExtension.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Security;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace SecureStringConsoleApplication
+{
+    public static class SecureStringExtensions
+    {
+        public static SecureString ToSecureString(this string password)
+        {
+            SecureString ss = new SecureString();
+            foreach (char c in password)
+            {
+                ss.AppendChar(c);
+            }
+
+            return ss;
+        }
+
+        public static SecureString FastConvertToSecureString(this string password)
+        {
+            if (password == null)
+                throw new ArgumentNullException("password");
+
+            unsafe
+            {
+                fixed (char* passwordChars = password)
+                {
+                    var securePassword = new SecureString(passwordChars, password.Length);
+                    securePassword.MakeReadOnly();
+                    return securePassword;
+                }
+            }
+        }
+
+        public static string ConvertToUnsecureString(this SecureString securePassword)
+        {
+            if (securePassword == null)
+                throw new ArgumentNullException("securePassword");
+
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+
+        // http://stackoverflow.com/questions/4502676/c-compare-two-securestrings-for-equality/4502736#4502736
+        public static Boolean SafeSecureStringEqual(this SecureString secureString1, SecureString secureString2)
+        {
+            if (secureString1 == null)
+            {
+                throw new ArgumentNullException("secureString1");
+            }
+            if (secureString2 == null)
+            {
+                throw new ArgumentNullException("s2secureString2");
+            }
+
+            if (secureString1.Length != secureString2.Length)
+            {
+                return false;
+            }
+
+            IntPtr ss_bstr1_ptr = IntPtr.Zero;
+            IntPtr ss_bstr2_ptr = IntPtr.Zero;
+
+            try
+            {
+                ss_bstr1_ptr = Marshal.SecureStringToBSTR(secureString1);
+                ss_bstr2_ptr = Marshal.SecureStringToBSTR(secureString2);
+
+                String str1 = Marshal.PtrToStringBSTR(ss_bstr1_ptr);
+                String str2 = Marshal.PtrToStringBSTR(ss_bstr2_ptr);
+
+                return str1.Equals(str2);
+            }
+            finally
+            {
+                if (ss_bstr1_ptr != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(ss_bstr1_ptr);
+                }
+
+                if (ss_bstr2_ptr != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(ss_bstr2_ptr);
+                }
+            }
+        }
+
+        public static Boolean UnsafeSecureStringEqual(this SecureString s1, SecureString s2)
+        {
+            if (s1 == null)
+            {
+                throw new ArgumentNullException("s1");
+            }
+            if (s2 == null)
+            {
+                throw new ArgumentNullException("s2");
+            }
+
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            IntPtr bstr1 = IntPtr.Zero;
+            IntPtr bstr2 = IntPtr.Zero;
+
+            RuntimeHelpers.PrepareConstrainedRegions();
+
+            try
+            {
+                bstr1 = Marshal.SecureStringToBSTR(s1);
+                bstr2 = Marshal.SecureStringToBSTR(s2);
+
+                unsafe
+                {
+                    for (Char* ptr1 = (Char*)bstr1.ToPointer(), ptr2 = (Char*)bstr2.ToPointer();
+                        *ptr1 != 0 && *ptr2 != 0;
+                         ++ptr1, ++ptr2)
+                    {
+                        if (*ptr1 != *ptr2)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+            finally
+            {
+                if (bstr1 != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(bstr1);
+                }
+
+                if (bstr2 != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(bstr2);
+                }
+            }
+        }
+
+    }
+}
